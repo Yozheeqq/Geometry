a6432e5355997502a704b407bc151bb107e51ccf
Yozheeqq <90212163+Yozheeqq@users.noreply.github.com>
2022-04-01 22:21:34 +0300

    Add files via upload

---
+++ GeometryTest/CircleTest.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Geometry;
+
+namespace GeometryTest
+{
+    [TestClass]
+    public class CircleTest
+    {
+        [TestMethod]
+        public void SquareRightTest()
+        {
+            double r = 5;
+            double expected = Math.PI * r * r;
+            double result = Circle.Square(r);
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void SquareZeroTest()
+        {
+            double r = 0;
+            double expected = 0;
+            double result = Circle.Square(r);
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void SquareNegativeTest()
+        {
+            double r = -5;
+            double result = Circle.Square(r);
+        }
+    }
+}
